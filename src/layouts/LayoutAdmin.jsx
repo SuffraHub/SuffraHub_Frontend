c7b7986fec0a5e65592363f53c29f7d6453c3568
mrstahuu <stanislaw.maik@gmail.com>
2025-07-29 11:46:09 +0200

    Some style changes 	modified:   src/components/admin/Header.jsx
    	modified:   src/layouts/LayoutAdmin.jsx

--- src/layouts/LayoutAdmin.jsx
+++ src/layouts/LayoutAdmin.jsx
@@ -9,11 +9,13 @@ const Layout = () => {
   return (
     <>
         <Header />
-        <div className="row">
-          <Sidebar />
-          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            <Outlet />
-          </main>
+        <div className="container-fluid">
+          <div className="row">
+            <Sidebar />
+            <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+              <Outlet />
+            </main>
+          </div>
         </div>
         <Footer />
     </>
