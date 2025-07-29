32ec3d109881218b854150d9eec3d7cd6fdeef61
mrstahuu <stanislaw.maik@gmail.com>
2025-07-29 16:37:18 +0200

    Deleted directory to_rewrite

--- to_rewrite/default/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Dane dostępowe do bazy danych
-$db_host = 'localhost'; // Host bazy danych
-$db_name = 'qlkvrliynd_vote'; // Nazwa bazy danych
-$db_user = 'qlkvrliynd_vote'; // Nazwa użytkownika bazy danych
-$db_password = '4]-WhBQvGKPueZA'; // Hasło użytkownika bazy danych
-?>
