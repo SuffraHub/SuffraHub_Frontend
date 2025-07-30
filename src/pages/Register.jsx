1a10ea9dc2fda9239d0cc203be621861d58de688
Curso React <cursoreact@esmovias-MacBook-Air.local>
2025-07-30 11:49:34 +0200

    Connecting register to backend

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,52 @@
 
-
+import axios from 'axios';
+import { useState } from 'react';
 
 function Register() {
+
+    const [message, setMessage] = useState("");
+    const [formData, setFormData] = useState({
+        username: '',
+        imie: '',
+        nazwisko: '',
+        email: '',
+        confirm_email: '',
+        password: '',
+        confirm_password: ''
+    });
+
+    function registerCLick() {
+
+        if (formData.email != formData.confirm_email) {
+            setMessage("Confirm email does not match.")
+        }
+        else if (formData.password != formData.confirm_password) {
+            setMessage("Confirm password does not match.")
+        }
+        else {
+            const dataToSend = {
+                username: formData.username,
+                name: formData.imie,
+                surname: formData.nazwisko,
+                email: formData.email,
+                password: formData.password
+            };
+
+            axios.post('http://localhost:8000/register', dataToSend)
+                .then((res) => setMessage(res.data.message))
+                .catch((err) => setMessage(err.response?.data?.message || "Unknown error"));
+        }
+    }
+
+
+    function handleChange(e) {
+        const { name, value } = e.target;
+        setFormData(prev => ({
+            ...prev,
+            [name]: value
+        }));
+    }
+
 
 
     return (
@@ -13,22 +58,21 @@ function Register() {
             </svg><b class="fs-2">Vote@maventplan</b>
             <h1 class="h3 mb-3 fw-normal">Rejestracja</h1>
             <form method="post">
-                <div class="form-floating mb-3"> <input type="text" class="form-control" id="username" placeholder="Nazwa użytkownika" name="username" /> <label for="username">Nazwa użytkownika</label> </div>
+                <div class="form-floating mb-3"> <input type="text" class="form-control" id="username" placeholder="Nazwa użytkownika" name="username" onChange={handleChange} /> <label for="username">Nazwa użytkownika</label> </div>
 
-                <div class="form-floating mb-1"> <input type="text" class="form-control" id="imie" placeholder="Imię" name="imie" /> <label for="imie">Imię</label> </div>
-                <div class="form-floating mb-3"> <input type="text" class="form-control" id="nazwisko" placeholder="Nazwisko" name="nazwisko" /> <label for="nazwisko">Nazwisko</label> </div>
+                <div class="form-floating mb-1"> <input type="text" class="form-control" id="imie" placeholder="Imię" name="imie" onChange={handleChange} /> <label for="imie">Imię</label> </div>
+                <div class="form-floating mb-3"> <input type="text" class="form-control" id="nazwisko" placeholder="Nazwisko" name="nazwisko" onChange={handleChange} /> <label for="nazwisko">Nazwisko</label> </div>
 
-                <div class="form-floating mb-1"> <input type="email" class="form-control" id="email" placeholder="E-mail" name="email" /> <label for="email">Email</label> </div>
-                <div class="form-floating mb-3"> <input type="email" class="form-control" id="email_check" placeholder="Powtórz E-mail" name="confirm_email" /> <label for="email_check">Powtórz E-mail</label> </div>
+                <div class="form-floating mb-1"> <input type="email" class="form-control" id="email" placeholder="E-mail" name="email" onChange={handleChange} /> <label for="email">Email</label> </div>
+                <div class="form-floating mb-3"> <input type="email" class="form-control" id="email_check" placeholder="Powtórz E-mail" name="confirm_email" onChange={handleChange} /> <label for="email_check">Powtórz E-mail</label> </div>
 
-                <div class="form-floating"> <input type="password" class="form-control" id="password" placeholder="Hasło" name="password" /> <label for="password">Hasło</label> </div>
-                <div class="form-floating"> <input type="password" class="form-control" id="password_check" placeholder="Potwierdź hasło" name="confirm_password" /> <label for="password_check">Potwierdź hasło</label> </div>
-
-                <button class="btn btn-primary w-100 py-2" type="submit">Zarejestruj się</button>
+                <div class="form-floating"> <input type="password" class="form-control" id="password" placeholder="Hasło" name="password" onChange={handleChange} /> <label for="password">Hasło</label> </div>
+                <div class="form-floating"> <input type="password" class="form-control" id="password_check" placeholder="Potwierdź hasło" name="confirm_password" onChange={handleChange} /> <label for="password_check">Potwierdź hasło</label> </div>
+                <span>{message}</span>
+                <input type="button" class="btn btn-primary w-100 py-2" onClick={() => registerCLick()} value="Zarejestruj się" />
             </form>
-            <span class="pt-2">Masz konto? <a href="login.php">Zaloguj się!</a></span>
+            <span class="pt-2">Masz konto? Zaloguj się!</span>
             <p class="mt-5 mb-3 text-body-secondary">&copy; mrstahuu - maventplan</p>
-
         </main>
     )
 }
