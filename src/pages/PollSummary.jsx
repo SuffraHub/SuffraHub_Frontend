b0d74d27cf94487422e186989b43c0d39c606eca
mrstahuu <stanislaw.maik@gmail.com>
2025-08-04 09:10:21 +0200

    PollSUmmary page chgd

--- src/pages/PollSummary.jsx
+++ src/pages/PollSummary.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
+// DODANO
 function PollSummary() {
   const navigate = useNavigate();
   const [answers, setAnswers] = useState({});
